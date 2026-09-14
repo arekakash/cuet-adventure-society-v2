f0a6632c967404a8c54eb40bec238da581567dcd
arekakash <arekakash@gmail.com>
2026-09-14 08:21:40 +0600

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="bn">
+      <body style={{ margin: 0, padding: 0, backgroundColor: '#f9fafb' }}>
+        {children}
+      </body>
+    </html>
+  )
+}
