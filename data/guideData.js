461b0ef3da9593b730692309b74947f483c5686c
arekakash <arekakash@gmail.com>
2026-09-15 09:47:08 +0600

    Update guideData.js

--- data/guideData.js
+++ data/guideData.js
@@ -1,9 +1,15 @@
 // data/guideData.js
 import { part1 } from './curriculum/part1';
 import { part2 } from './curriculum/part2';
+import { part3 } from './curriculum/part3';
+import { part4 } from './curriculum/part4';
+import { part5 } from './curriculum/part5';
 
-// আমরা এখানে পার্ট ১ এবং পার্ট ২ কে একটি সিঙ্গেল অ্যারেতে যুক্ত করে দিচ্ছি
+// আমরা এখানে ৫টি পার্টকেই একটি সিঙ্গেল অ্যারেতে যুক্ত করে দিচ্ছি
 export const megaCurriculum = [
   ...part1,
-  ...part2
+  ...part2,
+  ...part3,
+  ...part4,
+  ...part5
 ];
