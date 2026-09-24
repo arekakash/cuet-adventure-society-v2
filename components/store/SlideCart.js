480d2e52aeda55e158489fb6e051e3ca6d892416
Osman Gani <arekakash@gmail.com>
2026-09-24 10:30:06 +0600

    Update SlideCart.js

--- components/store/SlideCart.js
+++ components/store/SlideCart.js
@@ -1,7 +1,8 @@
 "use client";
 import Image from "next/image";
 import Link from "next/link";
-import { useCartStore } from "@/lib/useCartStore";
+import { useCartStore } from "@/app/store/useCartStore";
+
 
 export default function SlideCart({
   isCartOpen,
